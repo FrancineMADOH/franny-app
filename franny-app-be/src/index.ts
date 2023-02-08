a5caac169be16de82e2f52abc7377be19ed0a7cc
FrancineMADOH <francinemadoh@gmail.com>
2023-02-08 18:04:08 +0100

    initialize typescript prettier and eslint on this project

---
+++ franny-app-be/src/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 4000;
+app.use(bodyParser);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello Franny");
+});
+
+app.listen(port, () => {
+  console.log("Listening on port " + port);
+});
