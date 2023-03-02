ef431297d0eb9522e0b583ec04d487411b30e412
FrancineMADOH <francinemadoh@gmail.com>
2023-03-02 18:58:18 +0100

    added the requirement for the beauty section

--- franny-app-be/src/handlers/adminHandler.ts
+++ franny-app-be/src/handlers/adminHandler.ts
@@ -13,7 +13,7 @@ export class adminHandler {
       
         try{
             const {error} = signupValidation(req.body);
-            if(error) console.log(error); //return res.status(400).send(error.details[0].message);
+            if(error) return res.status(400).send(error.details[0].message);
             
             // if(!req.file){
             //     return res.status(400).send("Please upload a profile picture");
