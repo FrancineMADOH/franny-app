a69deaaeac638aad45c09a0210ba09cbe17b7d1e
FrancineMADOH <francinemadoh@gmail.com>
2023-04-05 11:36:36 +0100

    create shared components

--- frannyappfe/src/app/core/core.module.ts
+++ frannyappfe/src/app/core/core.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+import { AppSidebarComponent } from './app-sidebar/app-sidebar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AppHeaderComponent,
+    AppFooterComponent,
+    AppSidebarComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    AppHeaderComponent,
+    AppFooterComponent,
+    AppSidebarComponent
+    
   ]
 })
 export class CoreModule { }
