36b24919600bcd44d84c4d542230e9c4ae1d877c
FrancineMADOH <francinemadoh@gmail.com>
2023-05-27 00:39:56 +0100

    added appointement list page and add appointment page

--- frannyappfe/src/app/beauty/rdv/rdv.component.ts
+++ frannyappfe/src/app/beauty/rdv/rdv.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-rdv',
   templateUrl: './rdv.component.html',
   styleUrls: ['./rdv.component.css']
 })
-export class RdvComponent {
+export class RdvComponent implements OnInit {
+
+  @ViewChild('searchRendezVous', {static:true}) searchRendezVous:any;
+  constructor(private router:Router){}
+
+  ngOnInit(): void {
+    
+  }
+  searchArticleByTerm(searchRendezVous:any){
+    this.searchRendezVous.reset();
+  }
+  goToNewAppointmentPage(){
+    this.router.navigate(['beauty/add-rdv']);
+  }
 
 }
