bfc5796a72e10568b9d33c88285ba14d3f78df72
FrancineMADOH <francinemadoh@gmail.com>
2023-06-01 05:56:45 +0100

    refactor and correct forms

--- frannyappfe/src/app/beauty/beauty.module.ts
+++ frannyappfe/src/app/beauty/beauty.module.ts
@@ -5,17 +5,13 @@ import { FaqFormComponent } from './partials/faq-form/faq-form.component';
 import { FaqCardComponent } from './partials/faq-card/faq-card.component';
 import { CoreModule } from '../core/core.module';
 import { RouterModule } from '@angular/router';
-import { SeanceCardComponent } from './partials/seance-card/seance-card.component';
-import { SeanceFormComponent } from './partials/seance-form/seance-form.component';
 import { BeautifyerCardComponent } from './partials/beautifyer-card/beautifyer-card.component';
 import { BeautifyerFormComponent } from './partials/beautifyer-form/beautifyer-form.component';
 import { PrestationFormComponent } from './partials/prestation-form/prestation-form.component';
 import { PrestationCardComponent } from './partials/prestation-card/prestation-card.component';
 import { RdvCardComponent } from './partials/rdv-card/rdv-card.component';
 import { RdvFormComponent } from './partials/rdv-form/rdv-form.component';
-import { ReviewFormComponent } from './partials/review-form/review-form.component';
 import { ReviewCardComponent } from './partials/review-card/review-card.component';
-import { SeanceComponent } from './seance/seance.component';
 import { PrestationComponent } from './prestation/prestation.component';
 import { ReviewComponent } from './review/review.component';
 import { BeautifyerComponent } from './beautifyer/beautifyer.component';
@@ -33,17 +29,13 @@ import { AddReviewComponent } from './add-review/add-review.component';
     FaqsComponent,
     FaqFormComponent,
     FaqCardComponent,
-    SeanceCardComponent,
-    SeanceFormComponent,
     BeautifyerCardComponent,
     BeautifyerFormComponent,
     PrestationFormComponent,
     PrestationCardComponent,
     RdvCardComponent,
     RdvFormComponent,
-    ReviewFormComponent,
     ReviewCardComponent,
-    SeanceComponent,
     PrestationComponent,
     ReviewComponent,
     BeautifyerComponent,
@@ -52,7 +44,7 @@ import { AddReviewComponent } from './add-review/add-review.component';
     AddRdvComponent,
     AddBeautifComponent,
     AddPrestationComponent,
-    AddReviewComponent
+    AddReviewComponent,
   ],
   imports: [
     CommonModule,
