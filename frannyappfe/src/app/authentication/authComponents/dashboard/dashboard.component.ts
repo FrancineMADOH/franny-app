24f6a0863f18e5ff51f37cb0fbd05edee88af0ec
FrancineMADOH <francinemadoh@gmail.com>
2023-07-01 13:40:59 +0100

    passes user data to the header component

--- frannyappfe/src/app/authentication/authComponents/dashboard/dashboard.component.ts
+++ frannyappfe/src/app/authentication/authComponents/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject,Observable } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,15 +10,17 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DashboardComponent implements OnInit {
   currentUser:any = {};
+  cemail = "";
 
   constructor(private auth: AuthService, private activateRoute:ActivatedRoute ){
 
     let  email:string|null = this.activateRoute.snapshot.paramMap.get("email");
     this.auth.getAdminInfos(email||"").subscribe(res=>{
       this.currentUser = res ;
+      this.auth.updatedEmail(this.currentUser.email);
+
     });
   }
-
   ngOnInit(): void {
    
   }
