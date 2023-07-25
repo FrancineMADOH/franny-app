aeb9c5621fa59e54b8c65ad9a6ca04676811d2c8
FrancineMADOH <francinemadoh@gmail.com>
2023-07-25 21:39:28 +0100

    admin add rdv

--- frannyappfe/src/app/beauty/models/rdv.ts
+++ frannyappfe/src/app/beauty/models/rdv.ts
@@ -1,15 +1,20 @@
+import { Timestamp } from "rxjs"
+
 export interface Rendezvous {
-    rdv_id?:number,
+    rdv_id?: number,
     client_name: string,
     client_phone: number,
     client_email: string,
     rdvdate: string,
     doneby: number,
     prestation: number,
+    category:string,
     rdvstate: string,
     rdvcode: string,
-    rdvtype: string,
+    rdv_price: string,
     ville: string,
     quartier: string,
     comments: string
+    // price:number,
+    created_at:Date
 }
