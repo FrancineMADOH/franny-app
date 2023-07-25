2dfcba9a604a860e5a6fdce25cef0b1c42d700b1
FrancineMADOH <francinemadoh@gmail.com>
2023-07-25 21:37:24 +0100

    admin rdv creation done

--- franny-app-be/migrations/sqls/20230304163333-prestations-up.sql
+++ franny-app-be/migrations/sqls/20230304163333-prestations-up.sql
@@ -1,10 +1,12 @@
 CREATE TABLE prestations(
     pres_id SERIAL PRIMARY KEY,
-    title VARCHAR(20),
-    price INT,
+    title VARCHAR(20) NOT NULL,
+    price INT NOT NULL ,
     duration VARCHAR(10),
     category VARCHAR(20),
     seance TEXT,
-    gold VARCHAR(100),
-    premium VARCHAR(100)
+    gold VARCHAR(255),
+    gold_price INT NOT NULL,
+    premium VARCHAR(255) NOT NULL,
+    premium_price INT NOT NULL
 );
