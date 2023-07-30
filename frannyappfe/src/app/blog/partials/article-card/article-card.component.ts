ac47b70c7a643513bc56903c7eb27a33401cd79a
FrancineMADOH <francinemadoh@gmail.com>
2023-07-30 19:03:18 +0100

    view article,all article

--- frannyappfe/src/app/blog/partials/article-card/article-card.component.ts
+++ frannyappfe/src/app/blog/partials/article-card/article-card.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Post, PostResult } from '../../models/post';
+import { BlogService } from '../../services/blog.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-article-card',
   templateUrl: './article-card.component.html',
   styleUrls: ['./article-card.component.css']
 })
-export class ArticleCardComponent {
+export class ArticleCardComponent implements OnInit {
+
+  post!:PostResult;
+  postList:PostResult[] = [];
+  filteredPostList:PostResult[] = [];
+
+  constructor(private blog:BlogService, private route:ActivatedRoute,private router:Router){
+    this.filteredPostList = this.postList;
+  }
+
+  ngOnInit(): void {
+       this.blog.getblogpostList().subscribe((data)=>{
+        data.map((post:any)=>{
+          this.postList.push(post);
+          console.log(this.postList)
+          return this.postList;
+        })});
+    
+  }
+
+  viewblogPost(slug:string){ 
+    this.router.navigate(['posts/view/' + slug])
+  }
+  editblogPost(id:number){}
+  deleteblogPost(id:number){}
 
 }
