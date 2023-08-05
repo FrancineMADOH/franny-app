fd8d38694d741c92805feb918cff4d845a8ee2ca
FrancineMADOH <francinemadoh@gmail.com>
2023-08-05 10:12:02 +0100

    edit blog post

--- frannyappfe/src/app/shared/shared.module.ts
+++ frannyappfe/src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EmptyComponent } from './empty/empty.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    EmptyComponent
+  ],
   imports: [
     CommonModule
   ]
