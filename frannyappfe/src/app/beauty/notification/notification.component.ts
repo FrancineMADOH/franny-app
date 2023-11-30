7b108d934459283bc2d9c4bb5e0a2917ace418fd
FrancineMADOH <francinemadoh@gmail.com>
2023-11-30 21:30:12 +0100

    ...some changes on notifications

--- frannyappfe/src/app/beauty/notification/notification.component.ts
+++ frannyappfe/src/app/beauty/notification/notification.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BeautyService } from '../services/beauty.service';
 
 @Component({
   selector: 'app-notification',
   templateUrl: './notification.component.html',
   styleUrls: ['./notification.component.css']
 })
-export class NotificationComponent {
+export class NotificationComponent implements OnInit {
+
+  isresolve = false;
+  notifications:any[] = [];
+  constructor(private beauty:BeautyService){}
+
+  ngOnInit(): void {
+    this.beauty.getnewNotif().subscribe((res)=>{
+      res.map((el:any) => this.notifications.push(el))
+      return this.notifications;
+    })}
+
+  resolveNotif(form:any){}
 
 }
