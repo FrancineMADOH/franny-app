160f6f42dcd3a776e30b9228dc3a731acd17eb47
FrancineMADOH <francinemadoh@gmail.com>
2024-01-04 15:15:42 +0100

    corrected the url for the access control origin

--- franny-app-be/src/index.ts
+++ franny-app-be/src/index.ts
@@ -36,7 +36,7 @@ app.use(cors(corsOptions))
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', "https://frannyapp-demo.netlify.app/");
+  res.setHeader('Access-Control-Allow-Origin', "https://frannyapp-demo.netlify.app");
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   // Request headers you wish to allow
