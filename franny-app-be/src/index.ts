fe22dcad3425e68c862cb526fb99cae1bb8c0af8
FrancineMADOH <francinemadoh@gmail.com>
2024-01-04 12:47:54 +0100

    cors

--- franny-app-be/src/index.ts
+++ franny-app-be/src/index.ts
@@ -31,7 +31,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json())
 
 app.use(cors({
-  origin: "https://frannyapp-demo.netlify.app/"
+  origin: "*"
 }
 ))
 app.options('*', cors())
