79c2b0c93e265ce221d1ec2c5137f7462d7486da
FrancineMADOH <francinemadoh@gmail.com>
2024-01-04 11:51:00 +0100

    updated the proxy, managed the cors

--- franny-app-be/src/index.ts
+++ franny-app-be/src/index.ts
@@ -30,18 +30,17 @@ app.use(bodyParser.urlencoded({extended:true}));
 //app.use(express.json());
 app.use(bodyParser.json())
 app.use(cors());
-
-app.use(function (request, response, next) {
-  response.header("Access-Control-Allow-Origin", "*");
-  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization');
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});
+    }
+    next();
 });
 
 //router
 app.use("/api", router);
-
-
-
 
 app.get("/", (req: Request, res: Response) => {
   res.render("index.html")
