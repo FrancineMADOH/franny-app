657b1c7dbf84398bc6579bf16b80a17625032337
FrancineMADOH <francinemadoh@gmail.com>
2024-05-23 06:38:04 +0100

    added a dotenv file and dotenv config

---
+++ frannyappfe/src/app/create-env.ts
@@ -0,0 +1,22 @@
+import * as fs from 'fs';
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const targetPath = './src/app/environment/env.ts';
+
+const envConfigFile = `
+export const environment = {
+  production: false,
+  baseUrl: '${process.env ['API_URL']}',
+};
+`;
+
+// Write the environment file
+fs.writeFileSync(targetPath, envConfigFile, { encoding: 'utf8' });
+
+
+
+
+console.log(`Environment files generated at ${targetPath}`);
+
--- frannyappfe/src/app/environment/env.ts
+++ frannyappfe/src/app/environment/env.ts
@@ -1,6 +1,5 @@
+
 export const environment = {
-    production: true,
-    baseUrl: "http://localhost:4000/api"
-    // baseUrl : "http://18.207.117.64:4000/api"
-    //baseUrl:"https://frannynetwork.onrender.com/api"
-}
+  production: false,
+  baseUrl: 'http://localhost:4000/api',
+};
