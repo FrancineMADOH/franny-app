16cf5f3ee3c2ca999929f72ca809bbe4a44d7c64
FrancineMADOH <francinemadoh@gmail.com>
2024-05-26 09:50:49 +0100

    cors again

--- franny-app-be/src/index.ts
+++ franny-app-be/src/index.ts
@@ -45,7 +45,7 @@ app.use(cors(corsOptions))
 
 app.use(function (req, res, next) {
   if(environment=='test'){
-  res.setHeader('Access-Control-Allow-Origin',  "https://frannyapp-demo.netlify.app"  );
+  res.setHeader('Access-Control-Allow-Origin',  "*"  );
   res.setHeader('Access-Control-Allow-Origin', String(process.env.FE_URL_TEST));
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
